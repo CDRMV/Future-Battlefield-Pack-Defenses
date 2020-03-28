818e497f08dee227a5d730fff6fbbbf7ff20d24f
CDRMV <46399086+CDRMV@users.noreply.github.com>
2020-03-28 18:45:43 +0100

    Upload Icon, mod_info.lua and mod_units.bp

---
+++ mod_info.lua
@@ -0,0 +1,17 @@
+name = "Future Battlefield Pack (F.B.P.) Defenses"
+uid = "5t3edt-btz6-9438-h6ui-9287zt47fds"
+version = 1.0
+copyright = "Copyright © 2019, CDRMV (aka CDR Deadalus)"
+description = "FAF Version 1.0 'Future Battlefield Pack Defenses' is a Submod for F.B.P. It adds several new Defense Buildings like Outposts, Fortresses and more to the Game."
+author = "CDRMV (aka CDR Deadalus)"
+url = "http://www.moddb.com/mods/fbp-future-battlefield-pack"
+icon = "/mods/Future Battlefield Pack Defenses/Icon.png" 
+selectable = true
+enabled = true
+exclusive = false
+ui_only = false
+requires = { }
+requiresNames = { }
+conflicts = { }
+before = { }
+after = { }
